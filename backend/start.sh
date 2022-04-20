9e7dbee187c70641352593591668f62e20c6442e
季大仙 <1104802845@qq.com>
2022-04-20 22:02:39 +0800

    update conf

---
+++ backend/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 先等MySQL服务启动
+while ! nc -z db 3306 ;
+do
+  echo "Waiting for the MySQL Server"
+  sleep 3
+done
+
+python manage.py makemigrations&&
+python manage.py migrate&&
+uwsgi /root/backend/uwsgi.ini&&
+# tail空命令，保证有一个任务在前台执行，防止容器退出
+tail -f /dev/null
+# 若用户在启动容器时加入了其他命令，那么将会在这里执行，提高拓展性。
+exec "$@"
