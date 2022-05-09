51aa417ed70a71fb44f8aa24ac5103b04cfd2a23
zxt539 <zhouxintong2000@163.com>
2022-05-09 14:22:01 +0800

    finish apis about task

--- backend/image_process/url.py
+++ backend/image_process/url.py
@@ -10,5 +10,5 @@ urlpatterns=[
     path("project/", ProjectSetView.as_view(), name="create_project"),
     path("project/<pk>/", ProjectDetailView.as_view(), name="detail_project"),
     path("task/", TaskSetView.as_view(), name="create_task"),
-    # path("task/<pk>/", TaskDetailView.as_view(), name="detail_task"),
+    path("task/<pk>/", TaskDetailView.as_view(), name="detail_task"),
 ]
