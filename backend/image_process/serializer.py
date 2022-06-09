c96ce2edcd1e6e7fd08a1f649902785874b1d790
costwen <1148796268@qq.com>
2022-06-09 21:53:59 +0800

    fix: fix url

--- backend/image_process/serializer.py
+++ backend/image_process/serializer.py
@@ -5,6 +5,9 @@ from .models import Image
 
 class ImageSerializer(serializers.ModelSerializer):
     create_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",read_only=True)
+    url = serializers.SerializerMethodField()
+    def get_url(self, obj):
+        return "+"+ obj.url
     class Meta:
         model = Image
         exclude = ("user",)
