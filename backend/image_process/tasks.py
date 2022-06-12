d861aa7244b23dab278ebf023131982fc838d14f
costwen <1148796268@qq.com>
2022-06-12 10:48:14 +0800

    fix: fix celery

--- backend/image_process/tasks.py
+++ backend/image_process/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from cmath import log
 import logging
+from unicodedata import name
 from backend import celery_app
 import time
 from backend.util import MapImageHelper
@@ -15,16 +16,32 @@ import os
 logger = get_task_logger(__name__)
 
 def retrieval(task):
-    if task.coordinate is None:
-        task.imageA = task.project.imageA
-    img_url = task.imageA
-    img_a = PIL.Image.open(img_url)
+    imageA = task.project.imageA
+    filename = imageA.url.split("/")[-1]
+    img_a = PIL.Image.open(os.path.join("./media/", filename))
+    if task.coordinate:
+        tl = task.coordinate["tl"]
+        br = task.coordinate["br"] 
+        crop_img = img_a.crop((tl[0], tl[1], br[0], br[1]))
+        H, W = crop_img.size
+        new_record = Image(user = task.user, H = H, W = W, type="task", name="")
+        filename = new_record.id + ".png"
+        crop_img.save(os.path.join("./media/", filename))
+        new_record.url = "/images/" + filename
+        new_record.save()
+        imageA = new_record
+        img_a = crop_img
+    task.imageA = imageA
     result_image, ratio = P.retrieval_predict(img_a)
-    filename = str(task.id) + ".png"
-    # task.analysis = {"retrieval": ratio}
-    task.mask = "./media/"+filename
-    task.status = "finished"
+    H, W = result_image.size
+    mask = Image(user=task.user, H = H, W= W, type="mask", name="")
+    filename = mask.id + ".png"
     result_image.save('./media/'+filename)
+    mask.url = "/images/" + filename
+    mask.save()
+    # task.analysis = {"retrieval": ratio}
+    task.mask = mask
+    task.status = "finished"
     task.save()
 
 def sort(task):
