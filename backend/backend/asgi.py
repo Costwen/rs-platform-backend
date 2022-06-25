0badc0b31a46814aef25681426598f6738098c21
季大仙 <1104802845@qq.com>
2022-06-25 22:08:09 +0800

    Update asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,18 +8,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from image_process.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from .channelsmiddleware import JwtAuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        # "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
             JwtAuthMiddlewareStack(
                 URLRouter(websocket_urlpatterns)
