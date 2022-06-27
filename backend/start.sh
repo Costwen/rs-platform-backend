8314128fdc89242d9763a69d95983888afbd2176
季大仙 <1104802845@qq.com>
2022-06-27 12:00:34 +0800

    init.sh change

--- backend/start.sh
+++ backend/start.sh
@@ -11,7 +11,7 @@ python manage.py migrate&&
 uwsgi /root/backend/uwsgi.ini&&
 #celery -A backend worker -l info&&
 /etc/init.d/celeryd start
-#daphne -p 9000 -b 0.0.0.0 backend.asgi:application
+daphne -p 9000 -b 0.0.0.0 backend.asgi:application
 # tail空命令，保证有一个任务在前台执行，防止容器退出
 tail -f /root/backend/requirements.txt
 # 若用户在启动容器时加入了其他命令，那么将会在这里执行，提高拓展性。
