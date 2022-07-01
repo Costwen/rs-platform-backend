37b7d2f5b2f18c2ccd6a305bb1500de241c34b95
季大仙 <1104802845@qq.com>
2022-07-01 18:20:12 +0800

    bug fixed

--- backend/backend/Config.py
+++ backend/backend/Config.py
@@ -17,9 +17,22 @@ class Config:
     constrast_param_path = "params/deploy_contrast/model.pdiparams"
 
     sort_category = ["Bare soil","Building","Pavement","Road","Vegetation","Water"]
-    sort_color_map = [217,161,70,82,94,113,46,196,182,52,58,64,115,169,66,162,224,240]
-    # ['#d9a146', '#525e71', '#2ec4b6', '#343a40', '#73a942', '#90e0ef']
+    sort_color_map = [0xd9,0xa1,0x46, 0x52,0x5e,0x71,0x2e,0xc4,0xb6, 0x34,0x3a,0x40,0x73,0xa9,0x42, 0x90,0xe0,0xef]
+    detection_label_list = ["plane","baseball-diamond","bridge","ground-track-field","small-vehicle",
+                            "large-vehicle","ship","tennis-court","basketball-court",
+                            "storage-tank","soccer-ball-field","roundabout","harbor","swimming-pool",
+                            "helicopter","container-crane"]
+    detection_color_list = [0xf9,0x41,0x44,0xf3,0x72,0x2c,0xf8,0x96,0x1e,0xf9,
+                            0x84,0x4a,0xf9,0xc7,0x4f,0x90,0xbe,0x6d, 0x43,0xaa,0x8b,
+                            0x4d,0x90,0x8e, 0x57,0x75,0x90,0x27,0x7d,0xa1,0x0d,0x30,
+                            0x82,0x88,0xda,0xe7,0x76,0xcd,0x65,0xff,0xc2,0x47,
+                            0xff,0x81,0x33, 0xeb,0x51,0x33]
 
+    '''
+    ['#f94144', '#f3722c', '#f8961e', '#f9844a', '#f9c74f', '#90be6d', '#43aa8b',
+     '#4d908e', '#577590', '#277da1', '#0d3082', '#88dae7', '#76cd65', '#ffc247',
+     '#ff8133', '#eb5133']
+    '''
     enable_gpu = False
 
     
